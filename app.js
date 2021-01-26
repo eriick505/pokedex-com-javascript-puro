e8fc950c10931c895d74dbe1bd0af8c0dde73d7f
unknown <santoserick@gmail.com>
2021-01-25 23:34:22 -0300

    Função que lida com o pokemon clicado e exibe suas informações no popup criada, resta adicionar o restante das informações referente ao pokemon dentro do popup

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const searchInput = document.querySelector('#search')
 const pokedex = document.querySelector('[data-js="pokedex"]')
 const searchContainer = document.querySelector('.searchContainer')
 const popup = document.querySelector('.popup')
+const popUpBox = document.querySelector('.popup__box')
 const popupCloseBtn = document.querySelector('.popup__close')
+const popupContent = document.querySelector('.popup__content');
 
 const API = 'https://pokeapi.co/api/v2/pokemon'
 
@@ -20,7 +22,7 @@ const listPokemonTemplateHTML = pokemons => {
 
     accumulator += `
       <li class="card ${typesInfo[0]}" onClick="fetchPokemonInfo(${id})">
-        <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="${name}" />
+        <img class="card-image" src="${getPokemonImageByID(id)}" alt="${name}" />
         <h2 class="card-title">${id}. ${name}</h2>
         <p class="card-subtitle">${typesInfo.join(' | ')}</p>
       </li>
@@ -28,6 +30,8 @@ const listPokemonTemplateHTML = pokemons => {
     return accumulator;
   }, '')
 }
+
+const getPokemonImageByID = ID => `https://pokeres.bastionbot.org/images/pokemon/${ID}.png`
 
 const insertPokemonsIntoDOM = pokemons => {
   pokedex.innerHTML = pokemons;
@@ -65,21 +69,91 @@ const pokemonFilter = event => {
   })
 }
 
+const fetchPokemonInfo = async id => {
+  const response = await fetch(`${API}/${id}`)
+  const pokemon = await response.json();
+
+  handleWithPopupInfo(pokemon)
+}
+
 const openPopup = () => popup.style.display = 'block'
 
 const closePopup = event => {
   const isElementHasClasses = ['popup', 'popup__close'] 
   const elementClassTarget = event.target.getAttribute('class')
+  const typePokemonClass = popUpBox.classList.item(1)
 
   isElementHasClasses.map(item => {
     if(elementClassTarget === item) {
       popup.style.display = 'none'
+      popupContent.innerHTML = ''
+      popUpBox.classList.remove(typePokemonClass)
       return
     }
   })
 }
 
-const fetchPokemonInfo = async id => {
+const createElement = (elementName, attributes) => {
+  const element = document.createElement(elementName)
+  const attributeAsArray = Object.entries(attributes)
+
+  attributeAsArray.forEach(([key, value]) => element.setAttribute(key, value))
+
+  return element;
+}
+
+const handleWithPopupInfo = pokemon => {
+  const typesInfo = pokemon.types.map(({ type }) => type.name)
+
+  const typesLi = typesInfo.reduce((acc, item) => {
+    acc += `<li>${item}</li>`
+    return acc;
+  }, '');
+  
+  const popUpHeader = createElement('div', {
+    class: 'popup__header'
+  })
+
+  const popUpBody = createElement('div', {
+    class: 'popup__body'
+  })
+
+  const popUpHeaderTemplate = `
+    <h2>${pokemon.name}</h2>
+    <ul class="types">
+      ${typesLi}
+    </ul>
+    <img 
+      src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" 
+      alt="${pokemon.name}">
+  `
+  const popUpBodyTemplate = `
+    <ul class="menu">
+      <li class="active">About</li>
+      <li>Moves</li>
+      <li>Evolutions</li>
+    </ul>
+
+    <div class="tables">
+      <table>
+        <tbody>
+          <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>marks</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `
+  popUpHeader.innerHTML = popUpHeaderTemplate
+  popUpBody.innerHTML = popUpBodyTemplate
+
+  popupContent.appendChild(popUpHeader)
+  popupContent.appendChild(popUpBody)
+
+  popUpBox.classList.add(typesInfo[0])
   openPopup()
 }
 
